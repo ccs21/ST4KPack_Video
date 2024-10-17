ece4b337a2f4d034279c42df53a6df073953f009
ccs21 <ccs2121@naver.com>
2024-10-18 03:50:53 +0900

    20241018

--- git_push.bat
+++ git_push.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m "20241018"
-step_commits=$(git log --oneline --reverse refs/heads/main | awk 'NR % 1000 == 0')
+set step_commits=$(git log --oneline --reverse refs/heads/main | awk 'NR % 1000 == 0')
 echo "$step_commits" | while read commit message; do git push origin +$commit:refs/heads/main; done
